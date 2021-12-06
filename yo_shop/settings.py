5a37eb54f5a54696a115735e3dabb6fae4e401f1
oybek703 <hhoybek@gmail.com>
2021-12-06 12:16:48 +0500

    media file add and serve added

--- yo_shop/settings.py
+++ yo_shop/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
