266b76d0f6e0ac1b762e83e550522058222b32da
oybek703 <hhoybek@gmail.com>
2021-12-14 18:38:46 +0500

    models created

--- yo_shop/urls.py
+++ yo_shop/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
     path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
